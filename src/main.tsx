37bd5f9714490ccf65aa81c3c67f3314a08fcfaa
David Johnston <davidajohnston2000@gmail.com>
2025-04-30 01:03:10 +0100

    Auth wrapper for standalone dev

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,18 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import {Auth0Provider} from "@auth0/auth0-react";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+      <Auth0Provider
+          domain={'dajohnston.eu.auth0.com'}
+          clientId={'IgckwgORISVWwBUI7BQSD2AtIlL2onsX'}
+          authorizationParams={{
+              redirect_uri: window.location.origin,
+          }}
+      >
     <App />
+      </Auth0Provider>
   </StrictMode>,
 )
